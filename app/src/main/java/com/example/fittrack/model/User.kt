d69399c73cfd0046eb28b8e5f14558c24db66e20
RicardoGomes07 <ricafixe01@gmail.com>
2026-09-07 15:43:44 +0100

    Introducing User into the app (just the class)

---
+++ app/src/main/java/com/example/fittrack/model/User.kt
@@ -0,0 +1,13 @@
+package com.example.fittrack.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey
+    val id: UUID = UUID.randomUUID(),
+    val name: String,
+    val password: String
+)
