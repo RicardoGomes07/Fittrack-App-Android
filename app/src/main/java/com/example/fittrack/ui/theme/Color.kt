5bc57c0c135ccb87c8bfb32f468595be482e7af2
RicardoGomes07 <ricafixe01@gmail.com>
2026-09-07 12:08:41 +0100

    Change the Colors and Theme of the App

--- app/src/main/java/com/example/fittrack/ui/theme/Color.kt
+++ app/src/main/java/com/example/fittrack/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package com.example.fittrack.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+object FitTrackColors {
+    val Background = Color(0xFF051424)
+    val SurfaceContainerLowest = Color(0xFF010F1F)
+    val SurfaceContainerLow = Color(0xFF0D1C2D)
+    val SurfaceContainer = Color(0xFF122131)
+    val SurfaceContainerHigh = Color(0xFF1C2B3C)
+    val SurfaceContainerHighest = Color(0xFF273647)
+    val OnSurface = Color(0xFFD4E4FA)
+    val OnSurfaceVariant = Color(0xFFC2CAB0)
+    val Outline = Color(0xFF8C947C)
+    val Primary = Color(0xFFCCFF80)
+    val OnPrimary = Color(0xFF213600)
+    val PrimaryContainer = Color(0xFFA3E635)
+    val OnPrimaryContainer = Color(0xFF416400)
+    val Secondary = Color(0xFF5DE6FF)
+    val OnSecondary = Color(0xFF00363E)
+    val SecondaryContainer = Color(0xFF00CBE6)
+    val OnSecondaryContainer = Color(0xFF00515D)
+    val SecondaryFixedDim = Color(0xFF2FD9F4)
+    val Error = Color(0xFFFFB4AB)
+}
